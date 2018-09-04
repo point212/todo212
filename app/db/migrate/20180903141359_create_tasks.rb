f362c4d87d5abcaf13578c27a69ff80398682407
point212 <point212@gmail.com>
2018-09-04 23:39:14 +0300

    migration references fix

--- app/db/migrate/20180903141359_create_tasks.rb
+++ app/db/migrate/20180903141359_create_tasks.rb
@@ -1,6 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
+      t.belongs_to :user, index: true
       t.integer :user_id
       t.string :summary
       t.string :status
