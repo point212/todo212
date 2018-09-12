5f6c45b9cab6598c641e5b7b32721244262c2128
point212 <point212@gmail.com>
2018-09-12 10:59:36 +0300

    task add/delete

--- app/app/controllers/tasks_controller.rb
+++ app/app/controllers/tasks_controller.rb
@@ -2,14 +2,37 @@ class TasksController < ApplicationController
   before_action :authenticate_user
 
   def show
-    @tasks = Task.all
+    @tasks = Task.where(user_id: get_current_user.id)
   end
 
   def create
-    Task.create params
+    task = Task.create task_params
+    task.user_id = get_current_user.id
+    if task.save 
+      redirect_to task_url
+    else
+      flash[:alert] = task.errors.messages
+      redirect_to new_task_url
+    end
   end
 
   def new
+    @task = Task.new
   end
+
+  def update
+  end
+  
+  def delete
+    task = Task.find(:id)
+    if task && get_current_user.id == task.user_id 
+      task.destroy
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:user_id, :summary, :status, :body)
+    end
 
 end
