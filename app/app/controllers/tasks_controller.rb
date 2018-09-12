fdf61bc4150ef489c011d7ca6042effd963f28a3
point212 <point212@gmail.com>
2018-09-12 11:31:24 +0300

    task edit & some rework on controller

--- app/app/controllers/tasks_controller.rb
+++ app/app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :authenticate_user
 
-  def show
+  def index
     @tasks = Task.where(user_id: get_current_user.id)
   end
 
@@ -9,7 +9,7 @@ class TasksController < ApplicationController
     task = Task.create task_params
     task.user_id = get_current_user.id
     if task.save 
-      redirect_to task_url
+      redirect_to tasks_url
     else
       flash[:alert] = task.errors.messages
       redirect_to new_task_url
@@ -20,7 +20,22 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    pp params
+    @task = Task.find(params[:id])
+  end
+
   def update
+    task = Task.find(params[:id])
+    pp task
+    redirect_to tasks_path if task.nil?
+    if task.update_attributes(task_params)
+      if task.save
+        redirect_to tasks_url
+      end
+    end
+    #redirect_to edit_task_url
+    
   end
   
   def delete
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'site#index'
   get 'login' => 'sessions#show_login_page'
   post 'login' => 'sessions#login'
   get 'logout' => 'sessions#logout'
   get 'signup' => 'users/signup'
   get 'site/index'
-  resource :task
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'site#index'
+  resources :tasks
+
 end
